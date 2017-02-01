720f2a7cc3e27e46e43ec4c4a1a57769740451fb
Subhashis Bhowmik <subhashis.b96@gmail.com>
2017-02-02 01:12:19 +0530

    Login n Reg

--- php/confirm/index.php
+++ php/confirm/index.php
@@ -17,7 +17,7 @@ $result=sql($sql);
 if($result->num_rows>0){
     $sql="UPDATE `users` SET `confirmation`='0' WHERE `email`='$email'";
     //TODO:Make login
-    echo $sql;
+    sql($sql);
     $token=randomString(64);
 //    sql("DELETE FROM `cookie` WHERE 'email'='$email'");
     $result=sql("INSERT INTO `cookie` (`mail`,`token`) VALUES ('$email','$token')");
@@ -28,4 +28,4 @@ if($result->num_rows>0){
 }else{
     die('Invalid confirmation link');
 }
-//header('Location: ../../');
+header('Location: ../../');
