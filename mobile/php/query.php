f8df89cf485eff266f28596a02cb047e6a3796fe
Subhashis Bhowmik <subhashis.b96@gmail.com>
2017-02-11 10:16:44 +0530

    Admin Page

--- mobile/php/query.php
+++ mobile/php/query.php
@@ -13,7 +13,7 @@ if($email!='' && checkConvoAuth('')!=1) {
     clearAllCookies();
     die(-1);
 }
-if($email=='') $email='ANNONYMOUS';
+if($email=='') $email='ANONYMOUS';
 if(isset($_REQUEST['query']))$query=sanitizeString($_REQUEST['query']);
 if($query=='')die(0);
 sql("INSERT INTO `query` (`id`,`email`,`query`) VALUES (NULL,'$email','$query')");
