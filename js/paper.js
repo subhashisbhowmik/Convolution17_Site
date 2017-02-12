49281ac938394232f617abb2723e711e082e54a7
Subhashis Bhowmik <subhashis.b96@gmail.com>
2017-02-13 01:08:49 +0530

    BufferP

--- js/paper.js
+++ js/paper.js
@@ -3,7 +3,7 @@
  */
 var papierGone=false;
 var papierDetails=" By 2029, computers will have emotional intelligence and be convincing as people' -Ray Kurzweil,computer scientist. These advancements are possible with quality research works and innovative ideas.\nHave you performed any interesting experiment or do you have any such idea? Have you made any new model or prototypes?\nThis year Convolution 4.0, organised by Department of Electrical Engineering, Jadavpur University brings you a brand new event of Paper Presentation/Power Point Presentation. We encourage you to take part in this exciting event and place your innovative ideas.\nMake a write-up of 400 words about your own idea/experiments/prototypes and mail us at pconvolution@gmail.com on or before 20th February.\nNotification of selection for final: 25th February.";
-var buffer="";
+var bufferP="";
 var posi=0;
 
 function papierGo() {
@@ -32,7 +32,7 @@ function papier() {
 }
 
 function papierType(s) {
-    buffer=s;
+    bufferP=s;
     posi=0;
     pbufferType();
 
@@ -43,8 +43,8 @@ function papierType(s) {
 //     var x=buffer.charAt(posi++);
 //     // buffer=buffer.substr(1);
     var $left=$('#left');
-    $left.html(buffer.substring(0,x+1));
-    if(buffer.length>posi){
+    $left.html(bufferP.substring(0,x+1));
+    if(bufferP.length>posi){
         setTimeout(pbufferType,10);
     }
 }
