942d38c152cc683ed35bd4e0059179c08f368320
Subhashis Bhowmik <subhashis.b96@gmail.com>
2017-02-12 11:45:23 +0530

    Admin Details

--- admin/details.php
+++ admin/details.php
@@ -6,23 +6,77 @@
  * Time: 21:49
  */
 require_once "../php/functions.php";
-$user="";
-$pass="";
+$user = "";
+$pass = "";
 //print_r($_SESSION);
-if(isset($_SESSION['convo_admin_user']))$user=sanitizeString($_SESSION['convo_admin_user']);
-if(isset($_SESSION['convo_admin_pass']))$pass=sanitizeString($_SESSION['convo_admin_pass']);
-$result=sql("SELECT * FROM `admin` WHERE `user`='$user' AND `pass`='$pass'");
-if($result->num_rows==0) {
-    $user="";
-    $pass="";
-    $_SESSION['convo_admin_user']='';
-    $_SESSION['convo_admin_pass']='';
-}else{
+if (isset($_SESSION['convo_admin_user'])) $user = sanitizeString($_SESSION['convo_admin_user']);
+if (isset($_SESSION['convo_admin_pass'])) $pass = sanitizeString($_SESSION['convo_admin_pass']);
+$result = sql("SELECT * FROM `admin` WHERE `user`='$user' AND `pass`='$pass'");
+if ($result->num_rows == 0) {
+    $user = "";
+    $pass = "";
+    $_SESSION['convo_admin_user'] = '';
+    $_SESSION['convo_admin_pass'] = '';
+} else {
     sql("UPDATE `admin` SET ts=CURRENT_TIMESTAMP WHERE `user`='$user' AND `pass`='$pass'");
 }
+$event = "";
+if (isset($_REQUEST['event'])) $event = urldecode(sanitizeString($_REQUEST['event']));
+//die( $event." ".$user. ' '.$pass);
+if ($event == "" || $user == "" || $pass == "") header("Location: ./");
+
+$tag = '';
+$error = '';
+$selector = "`users`.`id` AS SignupID, `registration`.`id` AS RegID, event AS Event,`users`.`email`, `name` as `Name`, contact as Phone,class AS Class, dept AS Department, inst AS Institution, confirmation";
+$filename = 'Convo17 ' . str_replace(" ", "_", $event) . ' ' . date(' g:iA d_M_Y', strtotime('+5 hour +30 minutes', strtotime(date("Y-m-d H:i:s"))));
+if ($event == "All Users") {
+    $tag = 'users';
+    $select = "SELECT `id` AS SignupID, email, `name` AS `Name`, contact AS Phone,class AS Class, dept AS Department, inst AS Institution, confirmation  FROM `$tag`";
+} else if ($event == "All Registrations") {
+    $tag = 'registration';
+    $select = "SELECT $selector FROM `registration` JOIN `users` ON `users`.`email`=`registration`.`email`;";
+
+} else if ($event == "Convolution Team") {
+    $tag = 'team';
+    $select = "SELECT * FROM `$tag`";
+
+} else if ($event == "All Users and Registrations") {
+    $select = "SELECT $selector FROM `registration` RIGHT JOIN `users` ON `users`.`email`=`registration`.`email`;";
+//    die($select);
+} else if ($event == "Queries") {
+    $select = "SELECT id AS QueryID,email, query as Query FROM `query`";
+} else {
+    $tag = str_replace(" ", "", strtolower($event));
+    $select = "SELECT $selector FROM `registration`  JOIN `users` ON `users`.`email`=`registration`.`email` WHERE `registration`.`event`='$tag'";
+}
+$export = sql($select);
+if ($conn->errno) $texts = "Database Doesn't exist yet!";
+else $texts = "This Database is EMPTY!!";
+$num = $export->num_rows;
+if ($export->num_rows == 0) die("
+
+<!DOCTYPE html>
+<html lang=\"en\">
+<head>
+    <meta charset=\"UTF-8\">
+    <meta http-equiv=\"X-UA-COMPATIBLE\" content=\"IE=edge\">
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
+    <meta name=\"description\" content=\"Admin Page for Convolution 2017\"/>
+    <meta name=\"author\" content=\"Subhashis Bhowmik\"/>
+    <title>Convolution Admin</title>
+
+    <link rel=\"stylesheet\" type=\"text/css\" href=\"../css/reset.css\"/>
+    <link rel=\"stylesheet\" type=\"text/css\" href=\"css/main.css\"/>
+
+</head>
+<body style=\"height:100vh\">
+<div id=\"content\">$texts</div>
+<a href=\"./\" id=\"back\"><img src=\"img/back.png\"/></a>
+</body>
+</html>");
+$fields = $export->fetch_fields();
 
 
-//if($user==""||$pass=="") header("Location: ./");
 ?>
 
 
@@ -38,10 +92,42 @@ if($result->num_rows==0) {
 
     <link rel="stylesheet" type="text/css" href="../css/reset.css"/>
     <link rel="stylesheet" type="text/css" href="css/main.css"/>
+    <link rel="stylesheet" type="text/css" href="css/table.css"/>
 
 </head>
 <body style="height:100vh">
-<div id="content">This page is under construction!!</div>
+<!--<div id="content">This page is under construction!!</div>-->
+<div id="superWrapper">
+    <h1>Total Entries:<?php echo $num; ?></h1>
+    <div id="tableWrapper">
+
+            <div class="tbl-content">
+                <table cellpadding="0" cellspacing="0" border="0">
+                <thead>
+                <?php
+                foreach ($fields as $field) {
+                    echo "<th>" . $field->name . "</th>";
+                }
+                ?>
+                </thead>
+            
+                <tbody>
+                <?php
+                foreach ($export as $row) {
+                    echo "<tr>";
+                    foreach ($row as $data) echo "<td>" . $data . "</td>";
+                    echo "</tr>";
+                }
+                ?>
+                </tbody>
+            </table>
+        </div>
+        </table>
+    </div>
+</div>
 <a href="./" id="back"><img src="img/back.png"/></a>
+<script type="text/javascript" src="js/table.js"></script>
+<script type="text/javascript" src="../js/jq.js"></script>
+
 </body>
 </html>
---
+++ admin/js/table.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Subhashis on 12-02-2017.
+ */
+$(window).on("load resize ", function() {
+    var scrollWidth = $('.tbl-content').width() - $('.tbl-content table').width();
+    $('.tbl-header').css({'padding-right':scrollWidth});
+}).resize();
