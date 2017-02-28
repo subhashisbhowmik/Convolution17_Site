3e13fc36f0bcdba9ef722dce28859c20c60b4bfb
Subhashis Bhowmik <subhashis.b96@gmail.com>
2017-02-28 20:35:31 +0530

    Algo Reg

--- mobile/php/register.php
+++ mobile/php/register.php
@@ -20,31 +20,14 @@ if ($x == 1) {
     if (sql("SELECT * FROM `registration` WHERE `email`='$email' AND `event`='$event'")->num_rows == 0) {
         sql("INSERT INTO `registration` (`id`,`email`,`event`) VALUES (NULL,'$email','$event')");
 //    echo "INSERT INTO `registration` (`id`,`email`,`event`) VALUES (NULL,'$email','$event')";
+        $body="";
+        $sub="";
         if($event=='algomaniac') {
-            $body = "Thanks for registering on Algomaniac, Convolution 2017. The online prelims of Algomaniac will be held on 28th Feb, 2017 on Hackerrank, from 8 pm to 9 pm.
+            $sub= "Algomaniac Registration";
+            $body = "Thanks for registering on Algomaniac, Convolution 2017. The online prelims of Algomaniac is over. The second offline prelim is on 4th March, Saturday.
 <br>
 The Rules and procedure for the prelims are as the following:
 <br>
-
-<b style='text-decoration: underline'>Create Your Team</b>
-<br>
-Since Algomaniac is a team contest, you need to register as a team. To register as a team please follow these steps : 
-<br>
-
-1. Log in to Hackerrank
-<br>
-2. Click on the RIGHT drop down beside your username
-<br>
-3. Click on SETTINGS from the drop-down
-<br>
-4. Your Account settings will appear on the screen. Click on TEAMS on the left side
-<br>
-5. Register your team with a decent team name and contest Algomaniac 4.0 Prelims I
-<br>
-6. Add your teammate. Maximum 2 persons allowed in a team.
-<br>
-
-<b style='text-decoration: underline'>Regulations:</b>
 
 <br>
 1. All the team members must be registered in Algomaniac on the site www.convolutionjuee.com
@@ -53,12 +36,19 @@ Since Algomaniac is a team contest, you need to register as a team. To register 
 <br>
 3. Selected few will directly qualify for Algomaniac final rounds. 
 <br>
-
-Prelims link: <a href='https://www.hackerrank.com/algomaniac-4-0-prelims'>https://www.hackerrank.com/algomaniac-4-0-prelims</a>";
-            ob_end_flush();
-            flush();
-            sendGMail($email, "Algomaniac Registration", $body);
+<br>
+For more info, use the details button on the <a href='http://www.convolutionjuee.com'>website</a>.
+";
+        }else{
+            $e=ucfirst($event);
+            $sub= "$e Registration";
+            $body = "You have successfully registered for $e, Convolution 2017. For event schedule, use the details button on the website";
         }
+
+
+        ob_end_flush();
+        flush();
+        sendGMail($email, $sub, $body);
         echo '1';
     }else echo '2';
     
